66730bcbd9afa9396f07b227a1f17b4e3e416666
Menta <simcictilen.gmail.com>
2023-04-26 17:58:33 +0200

    correzzioni file js

--- js/main.js
+++ js/main.js
@@ -6,19 +6,19 @@ let userName, userSurname, preferredColor;
 
 // Chiedo all'utente il suo nome   
 
-userName prompt(Inserisci il tuo nome);
+userName = prompt("Inserisci il tuo nome");
 
 
 // poi chiedo il suo cognome
 
-userSurname prompt(Inserisci il tuo cognome);
+userSurname = prompt("Inserisci il tuo cognome");
 
 // e poi  gli chiedo il suo colore preferito.
 
-preferredColor prompt(Inserisci il tuo colore preferito);
+preferredColor = prompt("Inserisci il tuo colore preferito");
 
 // Alla fine scrivo sulla pagina nomecognomecolorepreferito
 
-document.getElementById("element-id").innerHTML = (userName + UserSurname + preferreColor);
+document.getElementById("element-id").innerHTML = (userName + userSurname + preferredColor);
 
 console.log(userName, userSurname, preferredColor);
