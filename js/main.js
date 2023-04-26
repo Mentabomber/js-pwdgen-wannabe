1a79aa96aa042a18acfd85e215d16c1a7b9d96fb
Menta <simcictilen.gmail.com>
2023-04-26 17:55:05 +0200

    prova js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//variabili
+
+let userName, userSurname, preferredColor;
+
+
+
+// Chiedo all'utente il suo nome   
+
+userName prompt(Inserisci il tuo nome);
+
+
+// poi chiedo il suo cognome
+
+userSurname prompt(Inserisci il tuo cognome);
+
+// e poi  gli chiedo il suo colore preferito.
+
+preferredColor prompt(Inserisci il tuo colore preferito);
+
+// Alla fine scrivo sulla pagina nomecognomecolorepreferito
+
+document.getElementById("element-id").innerHTML = (userName + UserSurname + preferreColor);
+
+console.log(userName, userSurname, preferredColor);
